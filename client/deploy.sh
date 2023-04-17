4d2aa59df1fb192a75506b2b1cd7d2575af81d78
Jaye <jshin0219@gmail.com>
2023-04-17 21:45:49 +0900

    change home page

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,10 +1,10 @@
 echo "Swtiching to branch master"
-git checkout client
+git checkout client-prod
 
 echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -i ~/.ssh/my-ssh-key -r build/* jaye@34.168.137.190:/var/www/34.168.137.190/
+scp -i ~/.ssh/ssh-key -r build/* jaye@34.168.137.190:/var/www/34.168.137.190/
 
 echo "Done!"
